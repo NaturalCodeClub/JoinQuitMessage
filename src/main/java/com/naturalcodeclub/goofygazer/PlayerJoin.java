ac6efa38dfc7fa57a259ba5cd5e96a8cc809dcea
GoofyGazer <Bling1112@outlook.com>
2025-06-12 11:04:43 +0800

    欢迎语和退出语加入了nekopara、Limbus风格

--- src/main/java/com/naturalcodeclub/goofygazer/PlayerJoin.java
+++ src/main/java/com/naturalcodeclub/goofygazer/PlayerJoin.java
@@ -20,13 +20,13 @@ public class PlayerJoin implements Listener {
         Sites.put(1, "<blue>喵呜~ <light_purple><bold>{player}</bold></light_purple> 主人来啦！欢迎回家喵！</blue>");
         Sites.put(2, "<aqua>咦~ 是 <yellow><bold>{player}</bold></yellow> 主人耶，快来一起玩嘛喵♪</aqua>");
         Sites.put(3, "<light_purple><bold>{player}</bold></light_purple> 主人终于上线了喵！我等你好久啦~");
-        Sites.put(4, "<gold>欢迎欢迎~ <green>{player}</green> 主人~喵娘给你准备了小鱼干哦！</gold>");
-        Sites.put(5, "<yellow><bold>{player}</bold></yellow> 主人加入了游戏！今天也一起加油喵！ฅ^•ω•^ฅ");
-        Sites.put(6, "<blue>叮咚~ <bold>{player}</bold> 主人闪亮登场喵~！♥</blue>");
-        Sites.put(7, "<blue><bold>{player}</bold></blue> 主人回来啦喵~ 快让我抱抱~ (>^ω^<)");
+        Sites.put(4, "<gold>欢迎欢迎~ <green>{player}</green> 主人~猫猫给你准备了小鱼干哦！</gold>");
+        Sites.put(5, "<yellow><bold>{player}</bold></yellow> 主人加入了游戏！<shadow:aqua:0.5>今天也一起加油喵！ฅ^•ω•^ฅ");
+        Sites.put(6, "<blue>叮咚~ <bold><shadow:#FF5555>{player}</shadow></bold> 主人闪亮登场喵~！♥</blue>");
+        Sites.put(7, "<blue><bold>{player}</bold></blue> 主人回来啦喵~ (>^ω^<)");
         Sites.put(8, "<yellow>正义降临！<bold>{player}</bold><red>将把恶人引向平等的死亡！\n" +
-                "<blue>已至吾之时刻？<green>吾乃，{player}！");
-        Sites.put(9, "<light_purple><bold>哈欠……{player}</bold></light_purple> 可真是个勤恳的人啊。这个点是怎么让自己保持清醒的啊…？");
+                "<blue>已至吾之时刻？吾乃，<green>{player}！");
+        Sites.put(9, "<light_purple><bold>哈欠……</light_purple>{player}<blue><bold>可真是个勤恳的人啊。这个点是怎么让自己保持清醒的啊…？</bold></blue>");
 
     }
 
